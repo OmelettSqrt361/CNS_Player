cf3bc531f79237c5ec379e44174f377ad30c5524
OmelettSqrt361 <omeletton@gmail.com>
2026-01-22 19:01:42 +0100

    Full Verze 1
    Full Verze jedna
    Blbě se přepíná mezi hrama
    Nefuguje na telefonu idk

---
+++ CNS_Player/Assets/Scripts/DynamicScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicScreen : MonoBehaviour
+{
+    int lastWidth;
+    int lastHeight;
+
+    void Start()
+    {
+        lastWidth = Screen.width;
+        lastHeight = Screen.height;
+        ApplyResize();
+    }
+
+    void Update()
+    {
+        if (Screen.width != lastWidth || Screen.height != lastHeight)
+        {
+            lastWidth = Screen.width;
+            lastHeight = Screen.height;
+            ApplyResize();
+        }
+    }
+
+    void ApplyResize()
+    {
+        Screen.SetResolution(Screen.width, Screen.height, true);
+    }
+}
